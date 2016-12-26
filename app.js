f3d397bf91da82f6f29b097bf54be507136df0eb
Dait <daitdoce@gmail.com>
2016-12-26 14:23:51 +0100

    Prueba networking simple - Posicionex (x, y) + desconectar player basico

--- app.js
+++ app.js
@@ -26,15 +26,34 @@ serv.listen(2000); //El puerto al que escucha el servidor
 /*===========================================================================================================*/
 
 /*SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO*/
+var SOCKET_LIST = {};
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
-    console.log('socket connection'); //Cuando haya una conexión esta función será llamada
-    socket.on('happy', function(data){
-        console.log('the client is happy coz ' +data.reason);
-    })
+    socket.id = Math.random(); //CAMBIAR ESTO A ALGO MAS PRO
+    socket.x = 0;
+    socket.y = 0;
+    socket.number = "" + Math.floor(10 * Math.random());
+    SOCKET_LIST[socket.id] = socket;
 
-    socket.emit('serverMsg', {
-        msg: 'pepepepee!'
+    socket.on('disconnect', function(){
+        delete SOCKET_LIST[socket.id];
     })
-})
+});
 /*=================================================================================================================*/
+setInterval(function(){ //LOOP
+    var pack = [] //tendrá la información de cada player
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.x+=1;
+        socket.y+=1;
+        pack.push({
+            x:socket.x,
+            y:socket.y,
+            number:socket.number
+        });
+    }
+    for(var i in SOCKET_LIST){
+        socket.emit('newPos',pack);
+    }
+},1000/25); //Será llamado cada 40ms
