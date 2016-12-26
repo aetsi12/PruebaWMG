56e9234e41548cb0fdd4569e51a7f92af5ed779f
Dait <daitdoce@gmail.com>
2016-12-26 13:50:06 +0100

    Preparando el terreno 3 - SOCKET.IO

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
     +El cliente envia datos al servidor (Ex: input)
     +El servidor envia datos al cliente (Ex: Enemy position)*/
 
+
+/*EXPRESS====EXPRESS====EXPRESS====EXPRESS====EXPRESS====EXPRESS====EXPRESS====EXPRESS====EXPRESS====EXPRESS*/
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app); //Creamos el servidor
@@ -21,3 +23,18 @@ app.use('/client', express.static(__dirname + '/client'));
 //No se podría acceder, por ejemplo, a /server/archivoSecreto.xml
 
 serv.listen(2000); //El puerto al que escucha el servidor
+/*===========================================================================================================*/
+
+/*SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO====SOCKET.IO*/
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+    console.log('socket connection'); //Cuando haya una conexión esta función será llamada
+    socket.on('happy', function(data){
+        console.log('the client is happy coz ' +data.reason);
+    })
+
+    socket.emit('serverMsg', {
+        msg: 'pepepepee!'
+    })
+})
+/*=================================================================================================================*/
