00b7bf12a2fc4b8b7d9d954d3436fa07a86e791d
Dait <daitdoce@gmail.com>
2016-12-26 13:03:57 +0100

    Preparando el terreno 2 - EXPRESS

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+//Tipos de comunicación:
+/*File communication (EXPRESS):
+    +El cliente pide al servido un archivo (Ex: playerImg.png)
+
+            mywebsite.com   :2000       /client/playerImg.png
+    URL =   DOMAIN          PORT        PATH*/
+
+/*Package communication (Socket.io):
+    +El cliente envia datos al servidor (Ex: input)
+    +El servidor envia datos al cliente (Ex: Enemy position)*/
+
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app); //Creamos el servidor
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+//De esta forma solo se puede acceder '/' (y te redirecciona a /client/index) y a lo que hay dentro de '/client'
+//No se podría acceder, por ejemplo, a /server/archivoSecreto.xml
+
+serv.listen(2000); //El puerto al que escucha el servidor
